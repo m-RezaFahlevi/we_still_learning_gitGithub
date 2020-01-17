db7a1ef4bf2a241a583100fd5775c3849eab0be7
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-01-18 02:24:08 +0700

    Added a new .r files

---
+++ appendix_a.r
@@ -0,0 +1,9 @@
+#This is just an appendix a from 
+#R-intro by cran.r-project.org
+
+#Generate two pseudo random normarl vectors of
+#x and y-coordiates
+x <- rnorm(50)
+y <- rnorm(x)
+print(x)
+print(y)
