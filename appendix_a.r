cb223a3a337c5d8d3524749a10e8274540bf1d65
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-01-18 02:33:21 +0700

    Try to make a data frame x, w

--- appendix_a.r
+++ appendix_a.r
@@ -7,3 +7,17 @@ x <- rnorm(50)
 y <- rnorm(x)
 print(x)
 print(y)
+plot(x,y)
+
+#See which R object are now in the R workspace
+ls()
+
+#remove objects no longer needed. (Clean up)
+rm(x,y)
+
+#Generate a sequence x = {1, 2, 3, ... , 20}
+x <- 1:20
+w <- 1 + sqrt(x)/2  #A 'weight' vector of standard deviations
+print(x)
+print(w)
+
