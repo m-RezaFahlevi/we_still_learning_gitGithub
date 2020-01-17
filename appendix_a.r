a11b22f37f796ac4d3fba082d8d10fbfe14f6045
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-01-18 03:07:42 +0700

    Make a nonparametric local regression function

--- appendix_a.r
+++ appendix_a.r
@@ -28,3 +28,30 @@ fm <- lm(y~x, data=dummy) #lm stand for linear models
 #Fit a simple linear regression and look at the analysis.
 #With y to the left of the tilde, we are modelling y dependent on x
 print(summary(fm))
+
+#Sice we know the standard deviations, we can do a weighted regression
+fm1 <- lm(y~x, data=dummy, weight=1/w^2)
+print(summary(fm1))
+
+#Make the columns in the data frame visible as variables
+print(attach(dummy))
+
+lrf <- lowess(x, y) #Make a nonparametric local regression function
+plot(x,y,pch='+')
+print(lrf)
+lines(x, lrf$y, col='blue') #Add in the local regression
+abline(0, 1, lty=3) #The true regression line (intercept 0, slope 1)
+abline(coef(fm1), col='red')
+detach() #Remove data frame from the search path
+
+#A standard regression diagnostic plot to check for heteroscedasticity
+plot(
+	fitted(fm), resid(fm),
+	xlab='Fitted values', ylab='Residuals',
+	main='Residuals vs Fitted'
+)
+
+#A normal scores plot to check for skewness, kurtosis
+#and outliers
+qqnorm(resid(fm), main='Residuals Rankit Plot')
+rm(fm, fm1, lrf, x, dummy) #Cleaning up
