69fc57d2eda851bdff109e573e766772eed63eec
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-04-06 03:21:26 +0700

    relation by fact

---
+++ swi_prolog/relation_by_fact.pl
@@ -0,0 +1,8 @@
+parent(tom, bob). %Tom is a parent of Bob.
+parent(tom, liz). %Tom is a parent of Liz.
+parent(pam, bob). %Tom is a parent of Pam.
+parent(bob, ann). %Bob is a parent of Ann.
+parent(bob, pat). %Bob is a parent of Pat.
+parent(pat, jim). %Pat is a parent of Jim.
+
+/*Find X and Y sich that X is a parent of Y*/
