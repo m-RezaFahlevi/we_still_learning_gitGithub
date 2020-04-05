5ecefb472c42aa0a80eb5004cc0f04d4f1d3c975
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-04-06 00:53:54 +0700

    Programming for artificial_intelligence

---
+++ swi_prolog/fact.pl
@@ -0,0 +1,16 @@
+/*
+	In the example below, we are given some fact.
+	rika is a girls.
+	ika is a girls.
+	indah is a girls.
+	zaki is a boys.
+	zikri is a boys.
+	doni is ikbal's fathers.
+*/
+
+girls(rika).
+girls(ika).
+girls(indah).
+boys(zaki).
+boys(zikri).
+fathers(doni, ikbal).
