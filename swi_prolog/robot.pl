4990f345002146fd4fdbf9e80f6ee380af90684a
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-04-14 04:52:09 +0700

    Robot'w world according prolog

---
+++ swi_prolog/robot.pl
@@ -0,0 +1,12 @@
+%fact is added
+see(a,2,5).		%Block a is seen in xy-coordinate (2,5)
+see(d,5,5).		%Block d is seen in xy-coordinate (5,5)
+see(e,5,2). 	%Block e is seen in xy-coordinate (5,2)
+
+%more fact is added
+%on(Block,Object): Block is stnding on Object
+on(a,b).
+on(b,c).
+on(c,table).
+on(d,table).
+on(e,table).
