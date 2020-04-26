c202121ade7ad3428256915a32a1b714aa1d32d3
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-04-27 01:44:15 +0700

    List in prolog is added

---
+++ swi_prolog/learn_list.pl
@@ -0,0 +1,6 @@
+%Here we are try to understand list
+%in programming language (swi-prolog)
+
+p([1,2,3,4]).
+p([aomine,midorima,akashi,murasakibara,kise]).
+p([sasha,annie,connie,[armin,mikasa,eren]]).
