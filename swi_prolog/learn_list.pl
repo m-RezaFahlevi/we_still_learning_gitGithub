0325a05cab6c6d5a336b9f37db65aaf430a94d1e
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-04-27 02:55:06 +0700

    Adding concate, add, and del operation in list pl

--- swi_prolog/learn_list.pl
+++ swi_prolog/learn_list.pl
@@ -9,3 +9,14 @@ p([sasha,annie,connie,[armin,mikasa,eren]]).
 anggota([apple,sony,samsung,htc,motorolla]).
 anggota(X,[X|_]).
 anggota(X,[_|Y]):- anggota(X,Y).
+
+%Concatenation list
+conc([],L,L).
+conc([X|L1],L2,[X|L3]):- conc(L1,L2,L3).
+
+%Adding list using predicate add
+add(X,L,[X|L]).
+
+%Delete an item in list using predicate del
+del(X, [X|Tail],Tail).
+del(X,[Y|Tail],[Y|TailX]):- del(X,Tail,TailX).
