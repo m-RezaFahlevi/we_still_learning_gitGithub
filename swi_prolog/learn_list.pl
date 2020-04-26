3cd628b077072c31dee6d6d431937e2a22b1bb9e
m-RezaFahlevi <ivelhafazer666@gmail.com>
2020-04-27 02:06:12 +0700

    recursive linear search in prolog is added as features

--- swi_prolog/learn_list.pl
+++ swi_prolog/learn_list.pl
@@ -4,3 +4,8 @@
 p([1,2,3,4]).
 p([aomine,midorima,akashi,murasakibara,kise]).
 p([sasha,annie,connie,[armin,mikasa,eren]]).
+
+%Recursive searching in List
+anggota([apple,sony,samsung,htc,motorolla]).
+anggota(X,[X|_]).
+anggota(X,[_|Y]):- anggota(X,Y).
